959a38890fbdea529b34fb339448e266f2332f50
Vivek Ranjan <vivek.ranjan@sakhaglobal.com>
2022-01-02 18:31:09 +0530

    Odd even withpython

---
+++ MasaiEvenOrOdd.py
@@ -0,0 +1,6 @@
+# You are provided an integer. Your task is to write a program that prints "Even" (without quotes) if the number is even and "Odd" (without quotes) if the number is odd.
+n = int(input())
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
