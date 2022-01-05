1b1c3d1aba5851a68670dacba01747966a002d23
Vivek Ranjan <vivek.ranjan@sakhaglobal.com>
2022-01-05 22:55:15 +0530

    Rock paper scissors game with python in terminal

---
+++ rps_game/rps_game.py
@@ -0,0 +1,25 @@
+import random
+
+arr = ['rock', 'paper', 'scissors']
+
+user_input = input(f"what's yours_choice --> 'rock', 'paper' or 'scissors'? ").lower()
+
+computer_input = random.choice(arr)
+
+if computer_input == user_input:
+    print(f'computer-{computer_input}\nyours_choice-{user_input}\n------------------------\nTied')
+
+if computer_input=='rock' and user_input=='paper':
+    print(f'computer-->{computer_input}\nyours_choice-->{user_input}\n------------------------\nYou Won')
+elif computer_input=='paper' and user_input=='rock':
+    print(f'computer-->{computer_input}\nyours_choice-->{user_input}\n------------------------\nYou Lost')
+
+if computer_input=='scissors' and user_input=='rock':
+    print(f'computer-->{computer_input}\nyours_choice-->{user_input}\n------------------------\nYou Won')
+elif computer_input=='rock' and user_input=='scissors':
+    print(f'computer-->{computer_input}\nyours_choice-->{user_input}\n------------------------\nYou Lost')
+
+if computer_input=='paper' and user_input=='scissors':
+    print(f'computer-->{computer_input}\nyours_choice-->{user_input}\n------------------------\nYou Won')
+elif computer_input=='scissors' and user_input=='paper':
+    print(f'computer-->{computer_input}\nyours_choice-->{user_input}\n------------------------\nYou Lost')
