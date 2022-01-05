1ea8b2ae7f32872168800cd28a9abc1ed1efae88
Vivek Ranjan <vivek.ranjan@sakhaglobal.com>
2022-01-05 09:39:43 +0530

    random module

---
+++ python_concepts/random_module.py
@@ -0,0 +1,22 @@
+import random
+
+arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+random.shuffle(arr)
+
+print(arr)
+
+# ----------------------------------------------------------------
+
+
+rand_range=random.randrange(1,10)
+
+print(rand_range)
+
+# ----------------------------------------------------------------
+
+rand_arr = [1,2,3,4,5,6,7,8,9]
+
+random_numer =  random.choice(rand_arr)
+
+print(random_numer)
